23971c08560b3f3fb1e7f9dfae4625aa3e21ba48
ultrasev <i@ultrasev.com>
2025-03-11 15:26:56 +0800

    refactor: Remove unused StockAnalyzer class

--- analyzer.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-import pandas as pd
-from datetime import datetime, timedelta
-import numpy as np
-
-class StockAnalyzer:
-    def __init__(self):
-        self.conn = duckdb.connect('stocks.duckdb')
