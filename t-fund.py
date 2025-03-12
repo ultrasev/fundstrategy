31bc75967a87e8f98979d7a2ab7d18652de358d5
ultrasev <i@ultrasev.com>
2025-03-12 17:50:42 +0800

    feat: Add 7-day holding period for selling shares

--- t-fund.py
+++ t-fund.py
@@ -3,7 +3,7 @@ from app.data.fetch import fetch_fund_data
 from typing import List, Dict, Tuple
 from decimal import Decimal
 import codefast as cf
-
+from datetime import datetime, timedelta
 from abc import ABC, abstractmethod
 
 
@@ -32,6 +32,19 @@ class TStrategy(AbstractStrategy):
                  threshold_rate: float = 1) -> None:
         super().__init__(data, initial_shares, max_shares, sell_holds, threshold_rate)
 
+    def _can_sell(self, current_date: str, holds: List[Tuple[str, int]]) -> bool:
+        """
+        Check if there are any holdings older than 7 days that can be sold
+        """
+        current_dt = datetime.strptime(current_date, '%Y-%m-%d')
+        seven_days_ago = current_dt - timedelta(days=7)
+
+        for hold_date, _ in holds:
+            hold_dt = datetime.strptime(hold_date, '%Y-%m-%d')
+            if hold_dt <= seven_days_ago:
+                return True
+        return False
+
     def calculate(self, ) -> Tuple[Decimal, int]:
         date = self.data[0]['FSRQ']
         minimal_holds = self.initial_shares // self.sell_holds
@@ -46,7 +59,7 @@ class TStrategy(AbstractStrategy):
             'total_cost': total_cost,
             'total_shares': total_shares,
             'minimal_holds': minimal_holds,
-            'holds': holds
+            'price': float(self.data[0]['DWJZ'])
         })
 
         for i, item in enumerate(self.data[1:]):
@@ -58,17 +71,36 @@ class TStrategy(AbstractStrategy):
                 shares_to_buy = 1000
                 total_shares += shares_to_buy
                 total_cost += current_price * shares_to_buy
-                cf.info(
-                    f"Buy {shares_to_buy} shares at {current_price} on {date}")
+                holds.append((date, shares_to_buy))
+                msg = 'Buy {} shares at {} on {}, total_shares: {}'.format(
+                    shares_to_buy,
+                    current_price,
+                    date,
+                    total_shares
+                )
+                cf.info(msg)
 
             elif change_rate > self.threshold_rate and total_shares > self.initial_shares:
+                # Check if we have any holdings older than 7 days
+                if not self._can_sell(date, holds):
+                    cf.info(
+                        'Skip selling on {} - no holdings older than 7 days'.format(date))
+                    continue
+
                 shares_to_sell = 1000
+                holds.pop(0)
                 total_shares -= shares_to_sell
                 total_cost -= current_price * shares_to_sell
                 fee = round(current_price * shares_to_sell * 0.005, 2)
                 total_cost += fee
-                cf.info(
-                    f"Sell {shares_to_sell} shares at {current_price} on {date}, fee: {fee}")
+                msg = 'Sell {} shares at {} on {}, total_shares: {}, fee: {}'.format(
+                    shares_to_sell,
+                    current_price,
+                    date,
+                    total_shares,
+                    fee
+                )
+                cf.info(msg)
 
         return total_cost, total_shares, float(self.data[-1]['DWJZ'])
 
@@ -89,6 +121,7 @@ async def main():
     print("Strategy Results:")
     print("Total Cost: {:.4f}".format(cost))
     print("Total Shares: {}".format(shares))
+    print("Initial price: {:.4f}".format(float(data[0]['DWJZ'])))
     print("Average Cost per Share: {:.4f}".format(avg_cost))
     print("Profit: {:.4f}".format(profit))
     print("Profit Rate: {:.4f}%".format(profit_rate * 100))
